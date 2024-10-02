23af11c827c49056f37dc8724191e1f3c7670cf6
Cédric BULTEEL <cedric@oceanspy.com>
2024-10-02 23:24:59 +0200

    Improve installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if command -v go &>/dev/null; then
+    echo "Go is installed"
+else
+    echo "Go is not installed"
+    echo "Please install Go and try again"
+    exit 1
+fi
+
 go build -o build/whatwords whatwords.go
 
 mkdir -p $HOME/.oceanspy/whatwords
